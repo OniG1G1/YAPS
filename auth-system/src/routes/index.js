fbb07ab2ee6198b8d965185317397ec95d74bb99
Oni_G1G1 <gianluigi.petrosillo@yahoo.it>
2026-02-23 16:59:51 +0100

    Reworked routes.js

--- auth-system/src/routes/index.js
+++ auth-system/src/routes/index.js
@@ -1,14 +1,54 @@
-const routeModules = [
-  require("./UserRoutes"),
+// routes.js
+
+const authController = require("./controllers/authController");
+const pageController = require("./controllers/pageController");
+
+module.exports = [
+
+// Page routes
+
+    {
+        method: "GET",
+        path: "/",
+        handler: pageController.home
+    },
+    {
+        method: "GET",
+        path: "/login",
+        handler: pageController.loginPage
+    },
+    {
+        method: "GET",
+        path: "/signup",
+        handler: pageController.signupPage
+    },
+    /*
+    {
+        method: "GET",
+        path: "/dashboard",
+        requiresAuth: true,
+        handler: pageController.dashboard
+    },
+    */
+
+// API routes
+
+    {
+        method: "POST",
+        path: "/api/login",
+        handler: authController.login
+    },
+    {
+        method: "POST",
+        path: "/api/signup",
+        handler: authController.signup
+    },
+    /*
+    {
+        method: "GET",
+        path: "/api/session",
+        handler: authController.session
+    },
+    */
+
 ];
-
-function registerRoutes(router) {
-  routeModules.forEach(mod => mod(router));
-}
-
-module.exports = registerRoutes;
-
-// add other/misc routes
-
-// single routes file, don't refactor router
-// write routes file ...
