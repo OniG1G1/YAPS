7cfd87ed5253547bd6961c1c8b123f1d8fcc79ba
Oni_G1G1 <gianluigi.petrosillo@yahoo.it>
2026-08-10 12:14:20 +0200

    Initialize Node.js project

--- auth-system/src/models/User.js
+++
@@ -1,8 +0,0 @@
-class User { // only used once when creating new user
-  constructor(username, password) {
-    this.username = username;
-    this.password = password;
-  }
-}
-
-module.exports = User;
